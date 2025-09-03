3f335e52f9e6fd973c58a24310a85098d8512734
Nicolly Bastos <121044663+nicollybastos@users.noreply.github.com>
2025-09-02 22:35:37 -0300

    Create Begin01.c

---
+++ Begin01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main () {
+    int idade;
+    //int idade = 25;
+    int quantidade;
+    // int quantdade = 1;
+    float altura;
+    // int altura = 1.75;
+    double peso;
+    // double peso = 90.3;
+    char letra;
+    // char letra = 'A'
+    char nome[20];
+    // char nome[20] = "sergio";
+
+    // aribuindo o valores depois
+    idade = 25;
+    quantidade = 10;
+    altura = 1.75;
+    peso = 90.3;
+    letra = 'A';
+    nome[20] = "sergio";
+}
