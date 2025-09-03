abab05bf64dd12deca2b8bd540d52b5924b775eb
Nicolly Bastos <121044663+nicollybastos@users.noreply.github.com>
2025-09-02 22:30:36 -0300

    Update aula01.c

--- aula01.c
+++ aula01.c
@@ -1,1 +1,17 @@
+Aula 01 - Desenvolvendo Programação em C
+Conceitos fundamentais de programação em C, abordando a importância de variáveis, os tipos de dados, os operadores matemáticos e de atribuição, bem como a utilização das funções de entrada e saída (scanf e printf).
+
+Tópicos abordados
+Utilização de Variáveis e Tipos de Dados
+Compreensão de como declarar e manipular variáveis, e quais tipos de dados utilizar dependendo da necessidade (int, float, char, etc.).
+
+Operadores Matemáticos e de Atribuição
+Aplicação de operadores como +, -, *, /, %, e operadores de atribuição =, +=, -=, entre outros.
+
+Interação com o Usuário
+Como receber entradas do usuário via scanf e exibir resultados utilizando printf.
+
+Organização e Estrutura de Código
+Boas práticas para organizar o código em C, incluindo indentação, comentários e clareza na escrita.
+
 
