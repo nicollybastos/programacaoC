afd982e6e132a701694255b187e8c3dc4899f70f
Nicolly Bastos <121044663+nicollybastos@users.noreply.github.com>
2025-09-02 22:35:56 -0300

    Create Begin02.c

---
+++ Begin02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int idade;
+    printf("Digite sua idade:\n");
+    // input em c:
+    scanf("%d", &idade);
+    
+    printf("Sua idade é: %d\n anos", idade);
+    return 0;
+}
